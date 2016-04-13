b23a3fe70b62692c573d298e0c3b433d00657134
Tomas Escartin Gutierrez <tomas.escartin@metrica.es>
2016-04-13 10:33:27 +0200

    Card definition

--- source/app/components/card.jsx
+++ source/app/components/card.jsx
@@ -1,19 +1,57 @@
 import React, { Component } from 'react';
+import marked from 'marked';
 
 class Card extends Component {
+    constructor () {
+        super(...arguments);
+        this.state = { showDetails: false };
+    }
+    
+    toggleDetails() {
+        this.setState({showDetails: !this.state.showDetails});
+    }
+    
+    resolveCardDetails() {
+        if (!this.state.showDetails) return undefined;
+        
+        let cardDetails = (
+                <div className="card-details">
+                    <span dangerouslySetInnerHTML={{__html:marked(this.props.card.description)}}/>
+                </div>
+            );
+        return cardDetails;    
+    }
+    
+    resolveSideColor() {
+        let sideColor = {
+            position: 'absolute',
+            zIndex: -1,
+            top: 0,
+            bottom: 0,
+            left: 0,
+            width: 7,
+            backgroundColor: this.props.card.cardType.backgroundColor
+        };
+        
+        return sideColor;
+    }
+    
     render() {
+        let cardDetails = this.resolveCardDetails();
+        let sideColor = this.resolveSideColor();
+        
         return (
-            <div className = "card">
-                <div className = "card-title">{this.props.title}</div>
-                <div className = "card-details">{this.props.description}</div>
+            <div className="card">
+                <div style={sideColor}/>
+                <div className={ this.state.showDetails ? 'card-title card-title-is-open' : 'card-title' } onClick={this.toggleDetails.bind(this)}>{this.props.card.title}</div>
+                {cardDetails}
             </div>
         );
     }
 }
 
 Card.propTypes = {
-    title: React.PropTypes.string.isRequired,
-    description: React.PropTypes.string
+    card: React.PropTypes.object.isRequired
 };
 
 export default Card;
--- source/app/main.jsx
+++ source/app/main.jsx
@@ -2,4 +2,15 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import Card from './components/card';
 
-ReactDom.render(<Card title= "Test" description="Card description"/>, document.getElementById('root'));
+let cardType = {
+    id: 1,
+    name: 'feature',
+    backgroundColor: '#34a97b' 
+};
+let card = {
+    id: 1,
+    title: 'Test',
+    description: 'Card description',
+    cardType: cardType
+};
+ReactDom.render(<Card card={card} />, document.getElementById('root'));
